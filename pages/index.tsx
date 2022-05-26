e4d529f0282c74e618247fffbab7739679e3c53a
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-05-27 07:57:01 +0900

    fix title

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>OZ</title>
         <meta name="oz" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
